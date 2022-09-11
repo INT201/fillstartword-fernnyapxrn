0d2a9743d863dcfdb7936df892e6833b9fa5794b
fernnyapxrn <areeyapornchotising@gmail.com>
2022-09-11 22:28:44 +0700

    i cann

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,24 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+ 
+  if(startWord === null || startWord === undefined || word === null || word === undefined)
+  {return undefined} 
+  
+  else 
+  {
+    if(word.includes(startWord))
+    {return word}
+
+    else
+    {return String.prototype.concat(startWord + word)}
+  }
 }
+
 module.exports = fillStartWord
+
+// ให้เขียน Function ชื่อ fillStartWord (startWord, word) 
+// เพื่อรับข้อความใด ๆ โดยให้ return wordข้อความที่รับเข้ามาและเติม 
+// startWord ก่อนหน้าข้อความนั้น โดยถ้า word นั้นมี startword นั้นอยู่แล้ว 
+// ไม่ต้องเติม startword อีก โดยถ้า word มีค่า null หรือ undefined ให้ 
+// return เป็น undefined
